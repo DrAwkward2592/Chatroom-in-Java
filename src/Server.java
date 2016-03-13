7c4b95c1eacf7d06c6bd1d91ebc96099d62bbe7f
M.S.Adityan <msadityan@gmail.com>
2016-03-12 23:19:03 -0500

    Added initial files.

---
+++ src/Server.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Server extends JFrame{
+
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+}
